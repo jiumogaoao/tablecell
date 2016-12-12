f73bf0cd3a622c86975cfc5a94289c55d28e390b
jiumogaoao <394127821@qq.com>
2016-12-13 04:25:08 +0800

    Initial Commit

---
+++ tablecell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tablecell
+//
+//  Created by 高志强 on 2016/12/13.
+//  Copyright © 2016年 高志强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tablecell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tablecell
+//
+//  Created by 高志强 on 2016/12/13.
+//  Copyright © 2016年 高志强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
